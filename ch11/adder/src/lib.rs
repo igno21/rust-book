ebfb64063e9e8051f070e8877ec5c734f7ab14ef
Igno21 <gwmacd21@gmail.com>
2025-01-16 10:10:48 -0800

    testing for ch11 minigrep for ch12

---
+++ ch11/adder/src/lib.rs
@@ -0,0 +1,22 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+pub fn add_two(a: usize) -> usize {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn internal() {
+        let result = internal_adder(2, 2);
+        assert_eq!(result, 4);
+    }
+}
